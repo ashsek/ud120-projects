0f62d8dfde4c6efef884399da924b91444938d68
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-02-05 13:46:47 +0530

    add timing

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,15 +12,27 @@
     
 import sys
 from time import time
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+
+clf = GaussianNB()
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+y = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+z = accuracy_score(labels_test,y)
+print(z)
 
 
 
